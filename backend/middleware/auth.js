8b0fa506f6b0a0ea2fe217625e2ba55829f0d939
Henrynunez112 <henrynunez@pursuit.org>
2020-06-27 23:22:39 -0400

    "aud" firebase error

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,11 +1,11 @@
 const admin = require("../firebase");
 const checkFirebaseToken = async (req, res, next) =>{
     try{
-        const token = req.hearders.authToken;
+        const token = req.headers.authtoken;
         const decodedToken = await admin.auth().verifyIdToken(token);
         const uid = decodedToken.uid;
         req.user_id = uid;
-        next()
+        next();
     }catch(err){
         console.log("Code Broke, ", err);
         res.status(400).json({
