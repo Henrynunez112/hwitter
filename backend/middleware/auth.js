68949d0ccb1d5118c1092a3b8d83869785d27e22
Henrynunez112 <henrynunez@pursuit.org>
2020-06-27 22:14:41 -0400

    firebase added to backend

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+const admin = require("../firebase");
+const checkFirebaseToken = async (req, res, next) =>{
+    try{
+        const token = req.hearders.authToken;
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        const uid = decodedToken.uid;
+        req.user_id = uid;
+        next()
+    }catch(err){
+        console.log("Code Broke, ", err);
+        res.status(400).json({
+            message: "No authenthicated users"
+        })
+    }
+};
+
+module.exports = {
+    checkFirebaseToken
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const users = require("express").Router();
 const { createUser, fetchAllUsers } = require("../queries/users");
+//check for users who are completely logged in
+const { checkFirebaseToken } = require("../middleware/auth");
 
 users.post("/", createUser);
-users.get("/", fetchAllUsers);
+users.get("/", checkFirebaseToken, fetchAllUsers);
 module.exports = users;
