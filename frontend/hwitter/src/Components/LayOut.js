764e868a634a85677a4508cbac8a8f5dd76772a7
Henrynunez112 <henrynunez@pursuit.org>
2020-06-29 00:04:43 -0400

    navbar bootstrap

---
+++ frontend/hwitter/src/Components/LayOut.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const LayOut = (props) => {
+  return <Container>{props.children}</Container>;
+};
+export default LayOut;
---
+++ frontend/hwitter/src/Components/Navigation.js
@@ -0,0 +1,79 @@
+import React, { useContext } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import styled from "styled-components";
+import TwitterSearch from "./TwitterSearch";
+import { AuthContext } from "../Providers/AuthContext";
+// import { NavLink } from "react-router-dom";
+import Logo from "../Imgs/Twitter_Logo_WhiteOnImage.png";
+import LogOut from "./LogOut";
+// import "../Css/NavBar.css";
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #15212b;
+  }
+  .navbar-brand .navbar-nav .navbar-link {
+    color: #1da1f2;
+  }
+  &:hover {
+    color: #ffffff;
+  }
+`;
+
+function Navigation() {
+  // const { currentUser } = useContext(AuthContext);
+
+  const buttonDisplay = () => {
+    // if (currentUser) {
+    //   return (
+    //     <>
+    //     <Nav.Item><LogOut /></Nav.Item>
+    //       {/* <LogOut /> */}
+    //     </>
+    //   );
+    // } else {
+    //   return (
+    //     <>
+    //       <Nav.Item><Nav.Link href ="/signup">Sign Up</Nav.Link></Nav.Item>
+    //       <Nav.Item><Nav.Link href ="/login">Log In</Nav.Link></Nav.Item>
+
+    //         {/* <NavLink to={"/signup"}>Sign Up</NavLink>
+    //         <NavLink to={"/login"}>Log In</NavLink> */}
+
+    //     </>
+    //   );
+    // }
+  };
+  const imgWandH = {
+    width: "32px",
+    height: "32px",
+  };
+
+  return (
+    <>
+    <Styles>
+      <Navbar expand="lg">
+         <Navbar.Brand exact to={"/"}>
+          <img alt="twitter logo" src={Logo} style={imgWandH} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {/* 
+        <Navbar.Collapse id="basic-navbar-nav"> */}
+          <Nav className="mr-auto">
+            <Nav.Item>
+              <Nav.Link href="/search">
+                <TwitterSearch />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/users">Users</Nav.Link>
+            </Nav.Item>
+            {buttonDisplay()}
+            </Nav>
+
+      </Navbar>
+    </Styles>
+    </>
+  );
+}
+export default Navigation;   
