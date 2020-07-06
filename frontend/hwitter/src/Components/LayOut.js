76951045852fb83e1953f4472c5574f1bc722b1a
Henrynunez112 <henrynunez@pursuit.org>
2020-07-06 01:42:51 -0400

    hover effect on login and sign up css

--- frontend/hwitter/src/Components/LayOut.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-
-const LayOut = (props) => {
-  return <Container>{props.children}</Container>;
-};
-export default LayOut;
