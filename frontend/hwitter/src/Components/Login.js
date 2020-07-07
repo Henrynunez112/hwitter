f7b09c2b7808b8768f2687fd043a048a2e9071ca
Henrynunez112 <henrynunez@pursuit.org>
2020-07-07 01:35:00 -0400

    building css box model on login form

--- frontend/hwitter/src/Components/Login.js
+++ frontend/hwitter/src/Components/Login.js
@@ -8,7 +8,6 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-
   const history = useHistory();
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,47 +17,51 @@ const Login = () => {
       //sign in with firebase then change route
       history.push("/");
     } catch (err) {
-      console.log("YERRRRR", err)
+      console.log("YERRRRR", err);
     }
   };
   const imgWandH = {
     //creating a size for the twitter bird
     width: "43.9px",
     height: "44.9px",
-    position: "relative"
+    position: "relative",
   };
 
   return (
     <div className="loginContainer">
       <div className="loginLogin">
-      <img alt="twitter logo" src={Logo} style={imgWandH} />
+        <img alt="twitter logo" src={Logo} style={imgWandH} />
       </div>
       <div className="loginHeader">
-      <h1 className="loginHeader">Log In to Twitter</h1>
+        <h4>Log In to Twitter</h4>
       </div>
       <div className="loginForm">
-      <form onSubmit={handleSubmit}>
-        <label for="loginName">Phone, email, or username</label>
-        <br></br>
-        <input
-          type="text"
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          value={email}
-        />
-        <br></br>
-        <label for="loginPassword">Password</label>
-        <br></br>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          value={password}
-        />
-        <div className="loginButtonPage">
-        <input type="submit" value="Login" />
-        </div>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <div className="loginNameContainer">
+            <label for="loginName">Phone, email, or username</label>
+            <br></br>
+            <input
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              value={email}
+            />
+          </div>
+          <br></br>
+          <div className="loginPasswordContainer">
+            <label for="loginPassword">Password</label>
+            <br></br>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              value={password}
+            />
+          </div>
+          <div className="loginButtonPage">
+            <input type="submit" value="Login" />
+          </div>
+        </form>
       </div>
     </div>
   );
