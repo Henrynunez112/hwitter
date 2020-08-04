d840c6fa40d60f54a67fd719cab360661ea77f81
Henrynunez112 <henrynunez@pursuit.org>
2020-08-03 22:21:54 -0400

    queries to get all post has stopped working

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,7 +1,7 @@
--- DROP DATABASE IF EXISTS hwitter_auth;
--- CREATE DATABASE hwitter_auth;
+DROP DATABASE IF EXISTS hwitter_auth;
+CREATE DATABASE hwitter_auth;
 
--- \c hwitter_auth;
+\c hwitter_auth;
 
 DROP TABLE IF EXISTS hashtags;
 DROP TABLE IF EXISTS hweets;
@@ -34,16 +34,3 @@ CREATE TABLE hashtags
 
 
 
-
-
-
--- INSERT INTO users
---     (id, email, firstname, lastname)
--- VALUES(2, 'little.s@gmail.com', 'henry', 'nunez'),
---     (3, 'shorty.s@gmail.com', 'saul', 'nunez');
--- INSERT INTO hweets
---     (hweets_id, content)
--- VALUES(2, 'hello world');
--- INSERT INTO hashtags
---     (post_id, hweeter_id, hweet_tags)
--- VALUES(1, 3, 'word')
