eec7efe45e01a089eb60de1fc9891ae4e3cc4a00
Henrynunez112 <henrynunez@pursuit.org>
2020-08-05 15:22:17 -0400

    created css for SearchBar.js

--- frontend/hwitter/src/Components/SearchBar.js
+++ frontend/hwitter/src/Components/SearchBar.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import "../Css/SearchBar.css"
+import React from "react";
+import "../Css/SearchBar.css";
 
-const SearchBar = () =>{
-    return (
-        <div className="formContainer">
-                <div className="searchBar">
-                  <button type="submit" className="searchButton">
-                    <img
-                      alt="search icon"
-                      src="https://img.icons8.com/metro/26/000000/search.png"
-                      id="searchIcon"
-                    />
-                  </button>
-                  <input
-                    className="searchInput"
-                    type="text"
-                    placeholder="Search Twitter"
-                  />
-                </div>
-              </div>
-    )
-
-}
+const SearchBar = () => {
+  return (
+    <div className="formContainer">
+      <div className="searchBar">
+        <button type="submit" className="searchButton">
+          <img
+            alt="search icon"
+            src="https://img.icons8.com/metro/26/000000/search.png"
+            id="searchIcon"
+          />
+        </button>
+        <input
+          className="searchInput"
+          type="text"
+          placeholder="Search Twitter"
+        />
+      </div>
+    </div>
+  );
+};
 export default SearchBar;
