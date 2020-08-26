06387b506990ac2edcdee2b8c1dee5bda04b3fe5
Henrynunez112 <henrynunez@pursuit.org>
2020-08-25 23:06:41 -0400

    navbar css being started

--- frontend/hwitter/src/Components/NavBar.js
+++ frontend/hwitter/src/Components/NavBar.js
@@ -4,10 +4,10 @@ import { NavLink } from "react-router-dom";
 import Logo from "../Imgs/Twitter_Logo_WhiteOnImage.png";
 import SearchBar from "../Components/SearchBar";
 import { logOut } from "../Util/firebaseFunction";
-// import "../Css/NavBar.css";
+import "../Css/NavBar.css";
 
 export default function NavBar() {
-  
+
   const { currentUser } = useContext(AuthContext);
 
   const buttonDisplay = () => {
@@ -54,7 +54,7 @@ export default function NavBar() {
               </NavLink>
             </div>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" id="signUpId">
             <div className="signUpButton">
               <NavLink
                 to={"/signup"}
@@ -74,45 +74,12 @@ export default function NavBar() {
     height: "32px",
   };
 
-  //   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  //   <a class="navbar-brand" href="#">Navbar</a>
-  //   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  //     <span class="navbar-toggler-icon"></span>
-  //   </button>
-  //   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  //     <ul class="navbar-nav mr-auto">
-  //       <li class="nav-item active">
-  //         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-  //       </li>
-  //       <li class="nav-item">
-  //         <a class="nav-link" href="#">Link</a>
-  //       </li>
-  //       <li class="nav-item dropdown">
-  //         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  //           Dropdown
-  //         </a>
-  //         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-  //           <a class="dropdown-item" href="#">Action</a>
-  //           <a class="dropdown-item" href="#">Another action</a>
-  //           <div class="dropdown-divider"></div>
-  //           <a class="dropdown-item" href="#">Something else here</a>
-  //         </div>
-  //       </li>
-  //       <li class="nav-item">
-  //         <a class="nav-link disabled" href="#">Disabled</a>
-  //       </li>
-  //     </ul>
-  //     <form class="form-inline my-2 my-lg-0">
-  //       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-  //       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  //     </form>
-  //   </div>
-  // </nav>
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <NavLink exact to={"/"} className="navbar-brand">
         <img alt="twitter logo" src={Logo} style={imgWandH} />
+        <span id='brandTitle'>Hwitter</span>
       </NavLink>
       <button
         class="navbar-toggler"
@@ -125,8 +92,8 @@ export default function NavBar() {
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navUl">{buttonDisplay()}</ul>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul className="navbar-nav justify-content-end">{buttonDisplay()}</ul>
       </div>
     </nav>
   );
