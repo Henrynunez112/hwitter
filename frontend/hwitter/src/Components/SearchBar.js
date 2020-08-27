057818f671a88a88ae5813a10f4c5056344cb6ae
Henrynunez112 <henrynunez@pursuit.org>
2020-08-26 23:51:43 -0400

    search bar css created

--- frontend/hwitter/src/Components/SearchBar.js
+++ frontend/hwitter/src/Components/SearchBar.js
@@ -6,18 +6,16 @@ const SearchBar = () => {
   const history = useHistory();
   const [search, setSearch] = useState("");
 
-  const handleSearch = (e) =>{
+  const handleSearch = (e) => {
     e.preventDefault();
-    history.push(`search/${search}`)
-    setSearch("")
-  }
-
-
+    history.push(`search/${search}`);
+    setSearch("");
+  };
 
   return (
     <div className="formContainer">
-      <form onSubmit={handleSearch} className="formSearch">
-        <button type="submit" className="searchButton" onClick={handleSearch}>
+      {/* <form onSubmit={handleSearch} className="formSearch"> */}
+      {/* <button type="submit" className="searchButton" onClick={handleSearch}>
           <img
             alt="search icon"
             src="https://img.icons8.com/metro/26/000000/search.png"
@@ -30,8 +28,29 @@ const SearchBar = () => {
           placeholder="Search where the hashtag was used (no need for the #)"
           onChange={(e) => setSearch(e.target.value)}
           value={search}
+        /> */}
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <button className="btn btn-outline-secondary" type="button" onClick={handleSearch}>
+            <img
+              alt="search icon"
+              src="https://img.icons8.com/metro/26/000000/search.png"
+              id="searchIcon"
+            />
+          </button>
+        </div>
+        <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="search for hashtags"
+          aria-label=""
+          aria-describedby="basic-addon1"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
         />
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
