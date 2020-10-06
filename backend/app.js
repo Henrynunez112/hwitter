6ccc58059737ca9b7b2adf1abe49d8d30abfebd6
Henrynunez112 <henrynunez@pursuit.org>
2020-10-06 14:40:08 -0400

    || 3001

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const hashtagsRouter = require("./routes/Hashtags/Hashtags");
 const searchRouter = require("./routes/Search/Search")
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
